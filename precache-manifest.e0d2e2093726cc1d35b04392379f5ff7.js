9ebd0f87785980bb89812aa45b43cdbbc925f6a4
sooraj <soorj.rs7@gmail.com>
2020-07-20 21:18:52 +0530

    Updates

---
+++ precache-manifest.e0d2e2093726cc1d35b04392379f5ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778c6ce7a7f5e1c1775548dbfab49977",
+    "url": "/react-one-todo/index.html"
+  },
+  {
+    "revision": "778b3735bc2872be7b90",
+    "url": "/react-one-todo/static/css/main.93012078.chunk.css"
+  },
+  {
+    "revision": "008db17c16e542a46644",
+    "url": "/react-one-todo/static/js/2.4e93c919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-one-todo/static/js/2.4e93c919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778b3735bc2872be7b90",
+    "url": "/react-one-todo/static/js/main.6e122149.chunk.js"
+  },
+  {
+    "revision": "2758405aa11ea16938f3",
+    "url": "/react-one-todo/static/js/runtime-main.19efa16d.js"
+  }
+]);
